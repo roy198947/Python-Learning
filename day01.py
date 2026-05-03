237c78ffebdb50fdba5edc9d9c1f30c06a760b3b
Sayan Roy <sayan9898@gmail.com>
2026-05-02 23:52:31 -0500

    Day 1: variables, types, and first script

---
+++ day01.py
@@ -0,0 +1,10 @@
+my_name = "Sayan Roy"
+my_age = 36
+my_city = "Minneapolis"
+learning_goals = ['Guitar', 'Python', 'Energy']
+goals_text = ", ".join(learning_goals)
+print(f"My name is {my_name}. I'm {my_age} years old, and I live in {my_city}. This month I want to learn: {goals_text}")
+print(type(my_name))
+print(type(my_age))
+print(type(my_city))
+print(type(learning_goals))
